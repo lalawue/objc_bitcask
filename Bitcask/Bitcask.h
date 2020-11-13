6f5b573cfb85ff7662bfb8e7a7483d14d34d706e
lalawue <suchaaa@gmail.com>
2020-11-13 15:30:05 +0800

    Initial Commit

---
+++ Bitcask/Bitcask.h
@@ -0,0 +1,19 @@
+//
+//  Bitcask.h
+//  Bitcask
+//
+//  Created by lii on 2020/11/13.
+//  Copyright © 2020年 suchang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bitcask.
+FOUNDATION_EXPORT double BitcaskVersionNumber;
+
+//! Project version string for Bitcask.
+FOUNDATION_EXPORT const unsigned char BitcaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bitcask/PublicHeader.h>
+
+
